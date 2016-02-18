9a91f54d5f9bca30d7a433d73002cd276394796c
eddie duro and michael lor <michael.vang.1985@gmail.com>
2016-02-18 15:07:32 -0800

    add passing spec and functionality for years divisible by 12

--- src/Weekday.php
+++ src/Weekday.php
@@ -5,44 +5,105 @@
         {
             $lower_case_month = strtolower($input_month);
             $year_number = str_split($input_year);
-            $year_number = $year_number[3];
+            $year_number_last = $year_number[3];
+            $leap_year = $year_number[2] . $year_number[3];
+            // var_dump($leap_year);
             $month_value = 0;
             $final_day = 0;
+            $leap_year_remainder = 0;
 
             $month_array = array("january"=> 6, "february"=>2, "march"=>2, "april"=>5, "may"=>0, "june"=>3, "july"=>5, "august"=>1, "september"=>4, "october"=>6, "november"=>2, "december"=>4);
 
             $day_values = array("sunday" => 0, "monday" => 1, "tuesday" => 2, "wednesday" => 3, "thursday" => 4, "friday" => 5, "saturday" => 6);
 
-            foreach($month_array as $key => $value)
-            {
-                if($lower_case_month == $key)
-                {
-                    $month_value = $value;
-                }
-            }
-            $day_of_week = $month_value + $input_day + $year_number;
+            $leap_year_array = array("12" => 1, "24" => 2, "36" => 3, "48" => 4, "60" => 5, "72" => 6, "84" => 7, "96" => 8);
 
-            if ($day_of_week > 6)
+            if ((($input_year % 4 == 0) && ($input_year % 100 != 0)) || ($input_year % 400 == 0))
             {
-                $remainder = $day_of_week % 7;
-                foreach($day_values as $key => $value)
+                $day_values = array("sunday" => 1, "monday" => 2, "tuesday" => 3, "wednesday" => 4, "thursday" => 5, "friday" => 6, "saturday" => 7, "saturday" => 0);
+
+                $remainder = 0;
+                foreach($month_array as $key => $value)
                 {
-                    if( $remainder == $value ){
-                        $resulting_day = $key;
-                        return $resulting_day;
+                    if($lower_case_month == $key)
+                    {
+
+                        $month_value = $value;
+                        // var_dump($month_value);
+                    }
+                }
+                // $leap_year_remainder = $leap_year % 12;
+                foreach($leap_year_array as $key => $value){
+                    if($leap_year == $key){
+                        $leap_year_remainder = $value;
+                        var_dump($leap_year_remainder);
+                    }
+
+                }
+                if($leap_year_remainder > 6 && $leap_year_remainder != 0){
+                    $leap_year_remainder = $leap_year_remainder % 7;
+                }
+
+                $day_of_week = $month_value + $input_day + $leap_year_remainder;
+                // var_dump($day_of_week);
+
+                if ($day_of_week > 6)
+                {
+                    $remainder = $day_of_week % 7;
+                    // var_dump($remainder);
+                    // if($input_day == 29){
+                    //     $remainder = $remainder + 6;
+                    // }
+
+                    // $remainder = $remainder + 2;
+                    // var_dump($remainder);
+                    foreach($day_values as $key => $value)
+                    {
+                        if( $remainder == $value ){
+                            $resulting_day = $key;
+                            return $resulting_day;
+                        }
+                    }
+                } else {
+                    foreach($day_values as $key => $value)
+                    {
+                        if( $day_of_week == $value ){
+                            $resulting_day = $key;
+                            return $resulting_day;
+                        }
                     }
                 }
             } else {
-                foreach($day_values as $key => $value)
+                foreach($month_array as $key => $value)
                 {
-                    if( $day_of_week == $value ){
-                        $resulting_day = $key;
-                        return $resulting_day;
+                    if($lower_case_month == $key)
+                    {
+                        $month_value = $value;
+                    }
+                }
+                // var_dump($year_number_last);
+                $day_of_week = $month_value + $input_day + $year_number_last;
+
+                if ($day_of_week > 6)
+                {
+                    $remainder = $day_of_week % 7;
+                    foreach($day_values as $key => $value)
+                    {
+                        if( $remainder == $value ){
+                            $resulting_day = $key;
+                            return $resulting_day;
+                        }
+                    }
+                } else {
+                    foreach($day_values as $key => $value)
+                    {
+                        if( $day_of_week == $value ){
+                            $resulting_day = $key;
+                            return $resulting_day;
+                        }
                     }
                 }
             }
-            // return $day_of_week;
-
 
         }
     }
--- tests/WeekdayTest.php
+++ tests/WeekdayTest.php
@@ -3,13 +3,43 @@
 
     class WeekdayTest extends PHPUnit_Framework_TestCase
     {
-        function test_month()
+        // function test_month()
+        // {
+        //     //Arrange
+        //     $weekday = new Weekday;
+        //     $input = "august";
+        //     $input2 = "1";
+        //     $input3 = "2003";
+        //
+        //     //Act
+        //     $output = $weekday->findDay($input, $input2, $input3);
+        //
+        //     //Assert
+        //     $this->assertEquals("friday", $output);
+        // }
+        //
+        // function test_year()
+        // {
+        //     //Arrange
+        //     $weekday = new Weekday;
+        //     $input = "July";
+        //     $input2 = "4";
+        //     $input3 = "2002";
+        //
+        //     //Act
+        //     $output = $weekday->findDay($input, $input2, $input3);
+        //
+        //     //Assert
+        //     $this->assertEquals("thursday", $output);
+        // }
+
+        function test_leapyear_by_12()
         {
             //Arrange
             $weekday = new Weekday;
-            $input = "august";
-            $input2 = "1";
-            $input3 = "2003";
+            $input = "february";
+            $input2 = "29";
+            $input3 = "2036";
 
             //Act
             $output = $weekday->findDay($input, $input2, $input3);
@@ -18,13 +48,13 @@
             $this->assertEquals("friday", $output);
         }
 
-        function test_year()
+        function test_leapyear_by_4()
         {
             //Arrange
             $weekday = new Weekday;
-            $input = "July";
-            $input2 = "4";
-            $input3 = "2002";
+            $input = "february";
+            $input2 = "29";
+            $input3 = "2032";
 
             //Act
             $output = $weekday->findDay($input, $input2, $input3);
